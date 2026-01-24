94c9d2b8e03fa30a7bd1031d8705dbea5f684000
336-Daniel <e0850405309@uisrael.edu.ec>
2026-01-23 23:36:25 -0500

    Arquitectura frontend animal life y template

---
+++ src/main/java/com/uisrael/apiALconsumo/ApiALconsumoApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.apiALconsumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiALconsumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiALconsumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/apiALconsumo/ApiALconsumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.apiALconsumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiALconsumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
