95b8887929f60aae7464e916bb7308815ee146ac
CarlosCisneros-cmd <imlol711@gmail.com>
2026-01-30 21:35:37 -0500

    Guardar Paciente con su relacion con cliente

--- src/main/java/com/uisrael/apiALconsumo/modelo/dto/response/PacienteResponseDTO.java
+++ src/main/java/com/uisrael/apiALconsumo/modelo/dto/response/PacienteResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 public class PacienteResponseDTO {
 	private int idPaciente;
+	private String nombre;
 	private String raza;
 	private String sexo;
 	private String especie;
